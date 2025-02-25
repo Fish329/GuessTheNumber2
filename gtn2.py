f3c81f47b895917f8126353ad806f34423c91423
Fish329 <268188@burlcoschools.org>
2025-02-25 09:21:37 -0500

    Add files via upload

--- gtn2.py
+++ gtn2.py
@@ -1,4 +1,18 @@
 import random
+def leaderboard():
+    names=[]
+    f=open("Chrysscores.txt","r")
+    funclist=f.read().split(",")
+    print(funclist)
+    print(len(funclist
+    ))
+    for i in range(int(len(funclist)/2)):
+        names.append([int((funclist[0])),str(funclist[1])])
+        funclist.pop(0)
+        funclist.pop(1)
+    names.sort(reverse=True)
+    print (names)
+
 print("""
 ╔═╡ ╥ ╥ ╔══ ╔═╡ ╔═╡
 ║ ╗ ║ ║ ╠══ ╚═╗ ╚═╗
@@ -6,12 +20,12 @@ print("""
 ═╦═ ╥ ╥ ╔══
  ║  ╠═║ ╠══
  ╨  ╨ ╨ ╚══
-╔═╗ ╥ ╥ ╔╦╗ ╔═╗ ╔══ ╔═╗
-║ ║ ║ ║ ║║║ ╠═╣ ╠══ ║
+╔═╗ ╥ ╥ ╔╦╗ ╔═╗ ╔══ ╔═╕
+║ ║ ║ ║ ║║║ ╠═╣ ╠══ ║ 
 ╨ ╨ ╚═╝ ╨ ╨ ╚═╝ ╚══ ╨
 With Smiley Sam! ☺
 """)
-f = open("Scores.txt","r")
+f = open("Chrysscores.txt","r")
 input("Press Enter to start!")
 print("\n")
 print("☺ Hello! My name is Smiley Sam!")
@@ -20,16 +34,19 @@ print("")
 list = f.read().split(",")
 if yourName in list:
     print("☺ Oh! Welcome back!")
+    familiar=False
 else: 
     print("☺ It's nice to meet you!")
+    familiar=True
 print("☺ Let's play Guess the Number!")
-number=random.randrange(1,100)
+number=5 #random.randrange(1,100)
 guesses=0
 print("")
 print("☺ I'm thinking of a number between 1 and 100. Can you guess what it is?")
 while True:
     guesses += 1
-    yourGuess=input("☺ What's your guess? ")
+    print(guesses)
+    yourGuess=int(input("☺ What's your guess? "))
     if yourGuess==number: break
     elif yourGuess>number: 
         print("☺ Nope!","\n","☺ My number is lower than that!")
@@ -40,3 +57,15 @@ while True:
 print("☺ That's the one!")
 print("")
 decision=input("☺ Well done! Want me to save your score? (y/n)")
+if decision == "y" or decision == "Y":
+    with open("Chrysscores.txt", "a") as f:
+        if not familiar:
+            r=open("Chrysscores.txt","r")
+            if len(r.read())!=0:
+                f.write(",")
+            f.write(str(100-guesses+1))
+            f.write(",")
+            f.write(yourName)
+        else:
+            pass #TODO: find out how to locate player's name, then replace the score (which should be right before it in the list)
+leaderboard()
