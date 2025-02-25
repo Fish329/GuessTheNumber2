7b474c4cb8b371555d9019f6ea4944877fe5c270
Fish329 <268188@burlcoschools.org>
2025-02-25 10:32:13 -0500

    Add files via upload
    its scary how little things are going wrong

--- gtn2.py
+++ gtn2.py
@@ -1,19 +1,17 @@
 import random
-def leaderboard():
+familiar=False
+def leaderboard(): #leaderboard function
     names=[]
     f=open("ChrysScores.txt","r")
     funclist=f.read().split(",")
-    print(funclist)
-    print(len(funclist
-    ))
     for i in range(int(len(funclist)/2)):
-        names.append([int((funclist[0])),str(funclist[1])])
+        names.append([int(funclist[0]),str(funclist[1])])
         funclist.pop(0)
-        funclist.pop(1)
+        funclist.pop(0)
     names.sort(reverse=True)
     print (names)
 
-print("""
+print(""" 
 ╔═╡ ╥ ╥ ╔══ ╔═╡ ╔═╡
 ║ ╗ ║ ║ ╠══ ╚═╗ ╚═╗
 ╚═╝ ╚═╝ ╚══ ╞═╝ ╞═╝
@@ -24,31 +22,29 @@ print("""
 ║ ║ ║ ║ ║║║ ╠═╣ ╠══ ║ 
 ╨ ╨ ╚═╝ ╨ ╨ ╚═╝ ╚══ ╨
 With Smiley Sam! ☺
-""")
-f = open("ChrysScores.txt","r")
+""") #logo
+f = open("ChrysScores.txt","r") #prepare to read existing players
 input("Press Enter to start!")
 print("\n")
-print("☺ Hello! My name is Smiley Sam!")
+print("☺ Hello! My name is Smiley Sam!") #ask player's name
 yourName=input("What's your name? ")
-print("")
 list = f.read().split(",")
-if yourName in list:
+if yourName in list: #greet depending on if a score has already been saved for this person
     print("☺ Oh! Welcome back!")
-    familiar=False
+    familiar=True
 else: 
     print("☺ It's nice to meet you!")
-    familiar=True
-print("☺ Let's play Guess the Number!")
-number=5 #random.randrange(1,100)
-guesses=0
+    familiar=False
 print("")
+print("☺ Let's play Guess the Number!")
+number=random.randrange(1,100)
+guesses=0
 print("☺ I'm thinking of a number between 1 and 100. Can you guess what it is?")
 while True:
-    guesses += 1
-    print(guesses)
+    guesses += 1 #increase attempt count
     yourGuess=int(input("☺ What's your guess? "))
-    if yourGuess==number: break
-    elif yourGuess>number: 
+    if yourGuess==number: break #If the guess is right, stop game
+    elif yourGuess>number: #otherwise provide hint
         print("☺ Nope!","\n","☺ My number is lower than that!")
         print("")
     else:
@@ -59,13 +55,22 @@ print("")
 decision=input("☺ Well done! Want me to save your score? (y/n)")
 if decision == "y" or decision == "Y":
     with open("ChrysScores.txt", "a") as f:
-        if not familiar:
+        if not familiar: #if the player is new, add their name and score to the text file.
             r=open("ChrysScores.txt","r")
             if len(r.read())!=0:
                 f.write(",")
             f.write(str(100-guesses+1))
             f.write(",")
             f.write(yourName)
-        else:
-            pass #TODO: find out how to locate player's name, then replace the score (which should be right before it in the list)
+        else: #If the player has played before
+            f=open("ChrysScores.txt","r")
+            list = f.read().split(",")
+            if int(list[list.index(yourName)-1])<100-guesses+1: #update their score if it's higher than the previous one
+                list[list.index(yourName)-1]=100-guesses+1
+                g=open("ChrysScore.txt","w")
+                g.write("")
+                f=open("ChrysScores.txt","a")
+                for i in list: #convert list back to text file
+                    f.write(i)
+                    f.write(",")
 leaderboard()
