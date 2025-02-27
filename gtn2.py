482ccba00ee6165e508d6662342c6a4b0925f8f7
Fish329 <268188@burlcoschools.org>
2025-02-27 09:30:13 -0500

    Update gtn2.py

--- gtn2.py
+++ gtn2.py
@@ -10,11 +10,11 @@ def leaderboard(): #leaderboard function
         funclist.pop(0)
         funclist.pop(0)
     names.sort(reverse=True) #Sort list by highest score
-    if len(names)==0:
+    if len(names)==0: #If the leaderboard is empty, dont print it
         return
     print("")
-    print ("-+=GRANDEST GUESSERS:=+-") #TODO: make the leaderboard work correctly
-    if len(names)>10:
+    print ("-+=GRANDEST GUESSERS:=+-") #leaderboard title
+    if len(names)>10: #Draw leaderboard
         for i in range(10):
             print (i+1,":",names[i][0],"  ",names[i][1])
     else:
@@ -23,7 +23,17 @@ def leaderboard(): #leaderboard function
         else:    
             for i in range(len(names)):
                 print(i+1,":",names[i][0],"  ",names[i][1])
-while True:
+
+class User: #User class stuff
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def assign_score(self, score):
+        self.score = score
+
+while True: #main loop
     print(""" 
     ╔═╡ ╥ ╥ ╔══ ╔═╡ ╔═╡
     ║ ╗ ║ ║ ╠══ ╚═╗ ╚═╗
@@ -45,6 +55,7 @@ while True:
     print("\n")
     print("☺ Hello! My name is Smiley Sam!") #ask player's name
     yourName=input("What's your name? ")
+    user1=User(yourName, 0) #create user object
     list = f.read().split(",")
     if yourName in list: #greet depending on if a score has already been saved for this person
         print("☺ Oh! Welcome back!")
@@ -57,7 +68,7 @@ while True:
     number=random.randrange(1,100) #generate number
     guesses=0
     print("☺ I'm thinking of a number between 1 and 100. Can you guess what it is?")
-    while True:
+    while True: #Game logic
         guesses += 1 #increase attempt count
         yourGuess=int(input("☺ What's your guess? "))
         if yourGuess==number: break #If the guess is right, stop game
@@ -71,21 +82,24 @@ while True:
             print("")
     print("☺ That's the one!")
     print("")
+    Print("☺ You got",100-guesses+1,"points!")
+    user1.assign_score(100-guesses+1) #assign player score
     decision=input("☺ Well done! Want me to save your score? (y/n)")
     if decision == "y" or decision == "Y":
+        
         with open("ChrysScores.txt", "a") as f:
             if not familiar: #if the player is new, add their name and score to the text file.
                 r=open("ChrysScores.txt","r")
                 if len(r.read())!=0:
                     f.write(",")
-                f.write(str(100-guesses+1))
+                f.write(str(user1.score))
                 f.write(",")
-                f.write(yourName)
+                f.write(user1.name)
             else: #If the player has played before
                 f=open("ChrysScores.txt","r")
                 list = f.read().split(",")
-                if int(list[list.index(yourName)-1])<100-guesses+1: #update their score if it's higher than the previous one
-                    list[list.index(yourName)-1]=100-guesses+1
+                if int(list[list.index(user1.name)-1])<user1.score: #update their score if it's higher than the previous one
+                    list[list.index(user1.name)-1]=user1.score
                     g=open("ChrysScores.txt","w")
                     g.write("")
                     f=open("ChrysScores.txt","a")
